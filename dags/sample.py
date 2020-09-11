0d6e531231a67838d383d53ca6f2cee5535e380b
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-09-11 14:52:16 -0700

    added random_failed method

--- dags/sample.py
+++ dags/sample.py
@@ -1,6 +1,7 @@
 from airflow.models import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
+import random
 
 args = {
     'owner':'Elyse',
@@ -12,18 +13,22 @@ dag = DAG(dag_id='my_dag', default_args=args,schedule_interval=None)
 def run_this_func(**context):
     print("hi elyse")
 
-def always_fail(**context):
-    raise Exception('Exceptionnnnnn')
+def random_fail(**context):
+    if random.random() > .7:
+        raise Exception('Exceptionnnnnn')
+    print('not failed')
 
 with dag:
     run_this_task = PythonOperator(
         task_id = 'run_this',
-        python_callable=run_this_func,
+        python_callable=random_fail,
         provide_context=True
     )
     run_this_task2 = PythonOperator(
         task_id = 'run_this2',
-        python_callable=run_this_func,
+        python_callable=random_fail,
         provide_context=True
+        retries = 10,
+        retry_delay=timedelta(seconds=1)
     )
     run_this_task >> run_this_task2
