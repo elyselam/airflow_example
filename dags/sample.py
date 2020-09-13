034bb1bd6a86a5cf4f157addafcb968965389175
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-09-12 17:22:36 -0700

    added FileSensor

--- dags/sample.py
+++ dags/sample.py
@@ -1,34 +1,34 @@
+from airflow.contrib.sensors.file_sensor import FileSensor
 from airflow.models import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
-import random
+from datetime import timedelta
 
 args = {
-    'owner':'Elyse',
-    'start_date':days_ago(1)
+    'owner': 'Elyse',
+    'start_date': days_ago(1)
 }
 
-dag = DAG(dag_id='my_dag', default_args=args,schedule_interval=None)
+dag = DAG(dag_id='my_dag', default_args=args, schedule_interval=None)
 
-def run_this_func(**context):
+
+def say_hi(**context):
     print("hi elyse")
 
-def random_fail(**context):
-    if random.random() > .7:
-        raise Exception('Exceptionnnnnn')
-    print('not failed')
 
 with dag:
-    run_this_task = PythonOperator(
-        task_id = 'run_this',
-        python_callable=random_fail,
-        provide_context=True
+    sensing_task = FileSensor(
+        task_id='sensing_task',
+        filepath='test.txt',
+        fs_conn_id='my_file_system2',
+        poke_interval=10
     )
-    run_this_task2 = PythonOperator(
-        task_id = 'run_this2',
-        python_callable=random_fail,
-        provide_context=True
-        retries = 10,
+    task1 = PythonOperator(
+        task_id='say_hi',
+        python_callable=say_hi,
+        provide_context=True,
+        retries=10,
         retry_delay=timedelta(seconds=1)
     )
-    run_this_task >> run_this_task2
+
+    sensing_task >> task1
